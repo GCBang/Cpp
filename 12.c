a017a0bb2bc85f90f78ab099c622c89fc1149723
gcb <qkdrnrwi@naver.com>
2021-07-19 23:05:07 +0900

    commit

--- 12.c
+++ 12.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	//freopen("11input.txt", "rt", stdin);
+	freopen("11input.txt", "rt", stdin);
 	int n, sum=0, c=1, d=9, cnt=0;
 	scanf("%d", &n);
     while(sum + d< n ){
